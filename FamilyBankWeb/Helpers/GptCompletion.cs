3803047230a459653773f67e1e1feafb2629d523
MuathZaher <81557758+MuathZahir@users.noreply.github.com>
2023-04-28 20:32:44 +0400

    Created Chatbot

---
+++ FamilyBankWeb/Helpers/GptCompletion.cs
@@ -0,0 +1,36 @@
+﻿using OpenAI_API;
+using OpenAI_API.Chat;
+using OpenAI_API.Models;
+
+namespace FamilyBankWeb.Helpers
+{
+    public class GptCompletion
+    {
+        private static Conversation chat;
+
+        public static void Initialize(bool isLoggedIn)
+        {
+            var api = new OpenAIAPI("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"); // shorthand
+
+            chat = api.Chat.CreateConversation(new ChatRequest()
+            {
+                Model = Model.GPT4
+            });
+
+            chat.AppendSystemMessage("You are a helpful and professional chatbot at a family bank called FamBank. " +
+                                     "Always answer briefly, don't write big paragraphs." +
+                                     (isLoggedIn ? "If the user is logged in and asks about how much is safe to spend, " +
+                                     "reply with '*' only, nothing else." : "Recommend that the user creates an account"));
+        }
+
+        public static async Task<string> AskChatbot(string message)
+        {
+            if(chat == null)
+                Initialize(true);
+
+            chat.AppendUserInput(message);
+
+            return await chat.GetResponseFromChatbotAsync();
+        }
+    }
+}
---
+++ FamilyBankWeb/wwwroot/js/chatbot.js
@@ -0,0 +1,83 @@
+var INDEX = 0;
+function onLoad() {
+
+    $("#chat-circle").click(function () {
+        $("#chat-circle").toggle('scale');
+        $(".chat-box").toggle('scale');
+    })
+
+    $(".chat-box-toggle").click(function () {
+        $("#chat-circle").toggle('scale');
+        $(".chat-box").toggle('scale');
+    })
+}
+
+function onSubmit(obj) {
+    var msg = $("#chat-input").val();
+    if (msg.trim() == '') {
+        return false;
+    }
+    generate_message(msg, 'self');
+    obj.invokeMethodAsync("SendToChatbot", msg);
+}
+
+function chatbotReply(msg) {
+    generate_message(msg, 'user');
+}
+
+function generate_message(msg, type) {
+    INDEX++;
+    var str = "";
+    str += "<div id='cm-msg-" + INDEX + "' class=\"chat-msg " + type + "\">";
+    str += "          <div class=\"cm-msg-text\">";
+    str += msg;
+    str += "          <\/div>";
+    str += "        <\/div>";
+    $(".chat-logs").append(str);
+    $("#cm-msg-" + INDEX).hide().fadeIn(300);
+    if (type == 'self') {
+        $("#chat-input").val('');
+    }
+    $(".chat-logs").stop().animate({ scrollTop: $(".chat-logs")[0].scrollHeight }, 1000);
+}
+
+//function generate_button_message(msg, buttons) {
+//    /* Buttons should be object array
+//        [
+//        {
+//            name: 'Existing User',
+//            value: 'existing'
+//        },
+//        {
+//            name: 'New User',
+//            value: 'new'
+//        }
+//        ]
+//    */
+//    INDEX++;
+//    var btn_obj = buttons.map(function (button) {
+//        return "              <li class=\"button\"><a href=\"javascript:;\" class=\"btn btn-primary chat-btn\" chat-value=\"" + button.value + "\">" + button.name + "<\/a><\/li>";
+//    }).join('');
+//    var str = "";
+//    str += "<div id='cm-msg-" + INDEX + "' class=\"chat-msg user\">";
+//    str += "          <div class=\"cm-msg-text\">";
+//    str += msg;
+//    str += "          <\/div>";
+//    str += "          <div class=\"cm-msg-button\">";
+//    str += "            <ul>";
+//    str += btn_obj;
+//    str += "            <\/ul>";
+//    str += "          <\/div>";
+//    str += "        <\/div>";
+//    $(".chat-logs").append(str);
+//    $("#cm-msg-" + INDEX).hide().fadeIn(300);
+//    $(".chat-logs").stop().animate({ scrollTop: $(".chat-logs")[0].scrollHeight }, 1000);
+//    $("#chat-input").attr("disabled", true);
+//}
+
+//$(document).delegate(".chat-btn", "click", function () {
+//    var value = $(this).attr("chat-value");
+//    var name = $(this).html();
+//    $("#chat-input").attr("disabled", false);
+//    generate_message(name, 'self');
+//})
